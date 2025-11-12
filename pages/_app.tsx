9bf66f71c60fde2c3f9f36b7f436af8ad4de5c95
Rehoboth <137082978+Robinadev@users.noreply.github.com>
2025-11-11 18:22:55 -0800

    ready made for deployement

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
+// pages/_app.tsx
 import '../styles/globals.css';
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
+import { useEffect, useState } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  // Prevent hydration mismatch
+  if (!isMounted) {
+    return null;
+  }
+
   return <Component {...pageProps} />;
 }
